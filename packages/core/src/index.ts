003823e37d7e4f58e4a54a6ba5b6831a7e7d1b4c
Emile Fugulin <code@efugulin.com>
2021-03-03 21:41:46 -0500

    [CORE] Expose the signature methods

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,4 @@ export { Configuration } from './configuration';
 export { Aggregator } from './aggregator';
 export * as logger from '@graphmetrics/logger';
 export * as client from '@graphmetrics/client';
+export * from './signature';
